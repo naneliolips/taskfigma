85a16068535d10a5ff3fe13bc77ea58880d70b99
naneliolips <batuhaneroglu@okul@gmail.com>
2022-07-03 19:13:49 +0300

    update v4 -

--- src/components/form-card.js
+++ src/components/form-card.js
@@ -1,24 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './form-card.css';
-import Vector from '../assets/Vector.png';
-import Header from './fontsMenu';
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./form-card.css";
+import Vector from "../assets/Vector.png";
+import Header from "./fontsMenu";
 
 function Formcard() {
-
-
     const [fonts, setFonts] = useState([]);
     const [fontFamilyList, setFontFamilyList] = useState([]);
 
     const [selectedFont, setSelectedFont] = useState("");
-    const [form, setForm] = useState({ name: '', email: '', font: '' });
+    const [form, setForm] = useState({ name: "", email: "", font: "" });
 
     const handleChange = (event) => {
-        setForm({ ...form, [event.target.name]: event.target.value })
+        setForm({ ...form, [event.target.name]: event.target.value });
         console.log(form);
-    }
+    };
 
     const handleChange2 = (ev) => {
         setSelectedFont(ev.target.value);
@@ -26,8 +22,8 @@ function Formcard() {
     };
 
     useEffect(() => {
-
-        axios.get("https://apiv2.popupsmart.com/api/googlefont")
+        axios
+            .get("https://apiv2.popupsmart.com/api/googlefont")
             .then((response) => {
                 setFonts(response.data);
             });
@@ -39,6 +35,17 @@ function Formcard() {
         );
     }, [fonts]);
 
+    const [error, setError] = useState(false);
+    const handleBlur = (event) => {
+        if (event.target.validity.typeMismatch) {
+            setError("email");
+        } else if (event.target.value.trim() === "") {
+            setError("empty");
+        } else {
+            setError(false);
+        }
+        // console.log(event.target.validity)
+    };
 
     return (
         <div className="formcard-container">
@@ -47,27 +54,60 @@ function Formcard() {
             </div>
             <form>
                 <div className="adverbDiv">
-                    <p className="adverdText">Sign up for our newsletter and<br></br>get 15% off your first order!</p>
+                    <p className="adverdText">
+                        Sign up for our newsletter and<br></br>get 15% off your first order!
+                    </p>
                 </div>
                 <div className="form-div">
-                    <input name="name" value={form.name} onChange={handleChange} type="text" className="formText" placeholder="Your name"></input>
+                    <input
+                        name="name"
+                        value={form.name}
+                        required
+                        onChange={handleChange}
+                        type="text"
+                        className="formText"
+                        placeholder="Your name"
+                    ></input>
                 </div>
                 <div className="form-div">
-                    <input type="email" name="email" value={form.email} onChange={handleChange} className="formText" placeholder="Email"></input>
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        value={form.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className="formText"
+                        placeholder="Email"
+                    ></input>
+                    {error === "email" && (
+                        <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                            Invalid email adress!
+                        </p>
+                    )}
+                    {error === "empty" && (
+                        <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                            This field is required!
+                        </p>
+                    )}
                 </div>
                 <div className="form-div">
-                    <input type="hidden" name="font" value={form.font} />
-                    <Header handleChange={handleChange2} fonts={fonts} selectedFont={selectedFont} fontFamilyList={fontFamilyList} />
+                    <input type="hidden" required name="font" value={form.font} />
+                    <Header
+                        handleChange={handleChange2}
+                        fonts={fonts}
+                        selectedFont={selectedFont}
+                        fontFamilyList={fontFamilyList}
+                    />
                 </div>
-                <div className='buttonDiv'>
-                    <button className='button'>GET MY 30% OFF </button>
+                <div className="buttonDiv">
+                    <button className="button">GET MY 30% OFF </button>
                 </div>
-                <div className='imgDiv'>
-                    <img className='imgClose' src={Vector} alt='closeV'></img>
+                <div className="imgDiv">
+                    <img className="imgClose" src={Vector} alt="closeV"></img>
                 </div>
             </form>
         </div>
-
     );
 }
 
