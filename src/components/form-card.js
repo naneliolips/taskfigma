3fc6b8657063a8716ad42e478020ee6726194f93
naneliolips <batuhaneroglu@okul@gmail.com>
2022-07-03 13:34:09 +0300

    .

--- src/components/form-card.js
+++ src/components/form-card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './form-card.css';
 import Vector from '../assets/Vector.png';
-import axios from 'axios';  
+
 
 
 function formcard() {
-    
-    
+
+
     return (
-       <div className="formcard-container">
+        <div className="formcard-container">
             <div className="headerDiv">
                 <h1 className="headerText">NEW STUFF</h1>
             </div>
             <div className="adverbDiv">
-                 <p className="adverdText">Sign up for our newsletter and<br></br>get 15% off your first order!</p>
+                <p className="adverdText">Sign up for our newsletter and<br></br>get 15% off your first order!</p>
             </div>
             <div className="form-div">
                 <input type="text" className="formText" placeholder="Your name"></input>
@@ -23,17 +23,16 @@ function formcard() {
             </div>
             <div className="form-div">
                 <input type="text" className="formText" placeholder="Fonts"></input>
-            </div>  
-            <div className='buttonDiv'>
-                    <button className='button'>GET MY 30% OFF </button>
             </div>
-             <div className='imgDiv'>
+            <div className='buttonDiv'>
+                <button className='button'>GET MY 30% OFF </button>
+            </div>
+            <div className='imgDiv'>
                 <img className='imgClose' src={Vector} alt='closeV'></img>
-             </div> 
-       </div>   
-       
+            </div>
+        </div>
+
     );
-  }
-  
-  export default formcard;
-  
+}
+
+export default formcard;
